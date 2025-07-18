268033564e9723f6cefa43ef524ad2a2ad6adc68
SaraPasolini <saraalmeida3606@gmail.com>
2025-07-18 18:53:34 -0300

    Palíndromo Verificador

--- Prática-1/Program.cs
+++ Prática-1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+//Verificador de Palíndromo
+class Progrmam
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Escreva sua palavra : ");
+        string palavra = Console.ReadLine();
+
+        string palavrainertida = new string(palavra.Reverse().ToArray());
+
+        if (palavra != palavrainertida)
+        {
+            Console.WriteLine("Não é Palíndromo!");
+        }
+        else
+        {
+            Console.WriteLine("É Palíndromo!");
+        }
+        
+    }
+}
